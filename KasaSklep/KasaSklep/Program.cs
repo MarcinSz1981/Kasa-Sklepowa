94398389925c57c9a6e11bd0fc95e298887a8c6f
szalk <szalk@DESKTOP-FR9R0O3>
2022-02-13 15:10:55 +0100

    Dodaj pliki projektów.

---
+++ KasaSklep/KasaSklep/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace KasaSklepowa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                PrintMenu();
+            }
+        }
+
+        static void PrintMenu()
+        {
+            Console.WriteLine("WYBIERZ OPCJĘ:");
+            Console.WriteLine("1 => LISTA WSZYSTKICH ZAKUPÓW");
+            Console.WriteLine("2 => ZAKUPY");
+            Console.WriteLine("3 => ZAKOŃCZ PROGRAM");
+            Console.WriteLine("WYBIERZ 1, 2 LUB 3:");
+            string w = System.Console.ReadLine();
+            HandleChoice(w);
+        }
+
+        static void HandleChoice(string w)
+        {
+            if (w == "1")
+            {
+                Console.Clear();
+                ShopService service = new ShopService();
+                foreach (var p in service.Warehouse)
+
+                Console.WriteLine($"{p.Name}  Cena netto: {p.NettoPrice}  zł    Cena brutto: {p.BruttoPrice} zł");
+            }
+            else if (w == "2")
+            {
+                //Console.Clear();
+                Paragon();
+            }
+            else if (w == "3")
+            {
+                System.Environment.Exit(0);
+            }
+        }
+
+        static void Paragon()
+        {
+            decimal price = 0;
+            ShopService service = new ShopService();
+            Console.WriteLine("KOD KRESKOWY LUB WYDRUK PARAGONU(P) :");
+            while (true)
+            {
+                string kod = System.Console.ReadLine();
+
+                if (kod == "p" || kod == "P")
+                {
+                    decimal netto = 0;
+                    DateTime localTime = DateTime.Today;
+                    Console.Clear();
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("");
+                    Console.WriteLine("PARAGON");
+                    Console.WriteLine("");
+                    Console.WriteLine($"DATA ZAKUPU: {localTime.ToString("dd-MM-yyyy")} ");
+                    Console.WriteLine("---------------------------------");
+                    foreach (var p in service.Paragon)
+                    {
+                        Console.WriteLine($"{p.Name} | {p.BruttoPrice} zł");
+                        netto = netto + p.NettoPrice; 
+                    }
+                    Console.WriteLine("---------------------------------");
+                    Console.WriteLine("");
+                    Console.WriteLine($"DO ZAPŁATY: {price} zł");
+                    Console.WriteLine($"W TYM VAT: {price-netto} zł");
+
+                    System.Environment.Exit(0);
+                }
+
+                bool existingbarcode = false;
+                foreach (var p in service.Warehouse)
+                {
+                    if (p.Barcode == kod)
+                    {
+                        existingbarcode = true;
+                        price = price + p.BruttoPrice;
+                        service.Paragon.Add(new Product(p.Barcode, p.Name, p.NettoPrice));
+                        Console.Clear();
+                        Console.WriteLine(p.Name);
+                        Console.WriteLine("");
+                        Console.WriteLine($"CENA ŁĄCZNA: {price} zł");
+                        Console.WriteLine("");
+                        Console.WriteLine("KOD KRESKOWY LUB WYDRUK PARAGONU(P) :");
+                        break;
+                    }
+                }
+                if (!existingbarcode)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("NIE ZNALEZIONO PRODUKTU");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("KOD KRESKOWY LUB WYDRUK PARAGONU(P) :");
+                }
+
+            }
+        }
+    }
+}
+
+    
---
+++ KasaSklep/KasaSklep/ShopService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KasaSklepowa
+{
+    public class ShopService
+    {
+        public ShopService()
+        {
+            CreateWarehouse();
+        }
+        public List<Product> Warehouse { get; set; } = new List<Product>();
+        public List<Product> Paragon { get; set; } = new List<Product>();
+
+        private void CreateWarehouse()
+        {
+            Warehouse.Add(new Product("001", "Masło Extra", 6.50m));
+            Warehouse.Add(new Product("002", "Chleb wiejski", 4.50m));
+            Warehouse.Add(new Product("003", "Makaron Babuni", 9.20m));
+            Warehouse.Add(new Product("004", "Dżem truskawkowy", 8.10m));
+            Warehouse.Add(new Product("005", "Kiełbasa myśliwska", 29.00m));
+            Warehouse.Add(new Product("006", "Szynka konserwowa", 22.00m));
+            Warehouse.Add(new Product("007", "Chipsy paprykowe", 6.00m));
+            Warehouse.Add(new Product("008", "Serek wiejski", 3.50m));
+            Warehouse.Add(new Product("009", "Sól kuchenna", 2.70m));
+            Warehouse.Add(new Product("010", "Cukier kryształ", 3.20m));
+        }
+    }
+}
+
